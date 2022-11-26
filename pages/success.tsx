9ba370e760097b1bc0c9de001ffb2e9dc58b1a61
christopher miller <chris.miller@padoq.com>
2022-11-26 17:53:29 +0000

    Add Success

---
+++ pages/success.tsx
@@ -0,0 +1,11 @@
+import MainTemplate from "../templates/MainTemplate";
+
+const Success = () => {
+    return (
+        <MainTemplate>
+            <h1>Successfully Submited</h1>
+        </MainTemplate>
+    )
+}
+
+export default Success;
