e92997ce5a236e2ca8d45b02b25b341f40cf11b9
christopher miller <chris.miller@padoq.com>
2022-11-26 17:45:36 +0000

    Add Contact Form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,9 +1,37 @@
+import styles from '../styles/Contact.module.css';
 import MainTemplate from "../templates/MainTemplate";
 
 const Contact = () => {
   return (
     <MainTemplate>
-      <h1>Contact</h1>
+        <h1>Contact Me!</h1>
+        <form name="contact" action="/success" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
+            <p>
+                <label htmlFor="yourname">
+                    Your Name:
+                </label>
+                <br />
+                <input type="text" name="name" id="yourname" className={styles.input}/>
+            </p>
+            <p>
+                <label htmlFor="youremail">
+                    Your Email:
+                </label>
+                <br />
+                <input type="email" name="email" id="youremail" className={styles.input}/>
+            </p>
+            <p>
+                <label htmlFor="yourmessage">
+                    Message:
+                </label>
+                <br />
+                <textarea name="message" id="yourmessage" className={styles.textInput}></textarea>
+            </p>
+            <p>
+                <button type="submit" className={styles.send}>Send</button>
+            </p>
+        </form>
     </MainTemplate>
   );
 }
